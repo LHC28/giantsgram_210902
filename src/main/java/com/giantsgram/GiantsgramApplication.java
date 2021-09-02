bc19b340db89973940c1eb201daeec98642765a3
DESKTOP-HLUUASN\zzang <zzangth95@naver.com>
2021-09-02 17:35:48 +0900

    21.09.02 초기 세팅

---
+++ src/main/java/com/giantsgram/GiantsgramApplication.java
@@ -0,0 +1,13 @@
+package com.giantsgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiantsgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiantsgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giantsgram/GiantsgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giantsgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiantsgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
