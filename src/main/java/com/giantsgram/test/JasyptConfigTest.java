0ea6c00e63396d3fdb092ccf77f88b6ae6d6aed3
이힘찬 <zzangth95@naver.com>
2022-01-30 23:13:31 +0900

    22.01.30 에러나오는 상황 확인 및 조치

---
+++ src/main/java/com/giantsgram/test/JasyptConfigTest.java
@@ -0,0 +1,36 @@
+package com.giantsgram.test;
+
+public class JasyptConfigTest {
+	 @Test
+	    void jasypt(){
+	        String url = "jdbc:postgresql://localhost:5432/postgres";
+	        String username = "postgres";
+	        String password = "1234";
+
+	        String encryptUrl = jasyptEncrypt(url);
+	        String encryptUsername = jasyptEncrypt(username);
+	        String encryptPassword = jasyptEncrypt(password);
+
+	        System.out.println("encryptUrl : " + encryptUrl);
+	        System.out.println("encryptUsername : " + encryptUsername);
+	        System.out.println("encryptPassword" + encryptPassword);
+
+	        Assertions.assertThat(url).isEqualTo(jasyptDecryt(encryptUrl));
+	    }
+
+	    private String jasyptEncrypt(String input) {
+	        String key = "5678";
+	        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+	        encryptor.setAlgorithm("PBEWithMD5AndDES");
+	        encryptor.setPassword(key);
+	        return encryptor.encrypt(input);
+	    }
+
+	    private String jasyptDecryt(String input){
+	        String key = "5678";
+	        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+	        encryptor.setAlgorithm("PBEWithMD5AndDES");
+	        encryptor.setPassword(key);
+	        return encryptor.decrypt(input);
+	    }
+}
